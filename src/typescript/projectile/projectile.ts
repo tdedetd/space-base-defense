2b6eab97b527e3e04f971a916e0caa4cb16a7add
tdedetd <tdedetd@gmail.com>
2025-12-31 02:25:47 +0300

    Projectile intersects abstract

--- src/typescript/projectile/projectile.ts
+++ src/typescript/projectile/projectile.ts
@@ -3,8 +3,5 @@ import { Rectangle } from '../models/geometry/rectangle.interface';
 import { SceneObject } from '../scene-object';
 
 export abstract class Projectile extends SceneObject {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public intersects(figure: Line | Rectangle): boolean {
-    throw new Error('Method is not implemented');
-  }
+  public abstract intersects(figure: Line | Rectangle): boolean;
 }
